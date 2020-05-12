2f8e15f72b93d07b2354e138cd55263c63a4c412
tkma16v <rohan.sharma@kohls.com>
2020-05-12 17:54:54 +0530

    init databing and added recycler view dependency

--- app/src/main/java/com/raydevelopers/newvalley/ui/activities/AllChannelsActivity.kt
+++ app/src/main/java/com/raydevelopers/newvalley/ui/activities/AllChannelsActivity.kt
@@ -2,6 +2,8 @@ package com.raydevelopers.newvalley.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import com.raydevelopers.newvalley.R
 import com.raydevelopers.newvalley.ui.fragments.AllChannelsFragment
 
@@ -9,10 +11,10 @@ class AllChannelsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        setContentView(R.layout.all_channels_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, AllChannelsFragment.newInstance())
+                    .replace(R.id.fragment_container, AllChannelsFragment.newInstance())
                     .commitNow()
         }
     }
--- app/src/main/java/com/raydevelopers/newvalley/ui/fragments/AllChannelsFragment.kt
+++ app/src/main/java/com/raydevelopers/newvalley/ui/fragments/AllChannelsFragment.kt
@@ -1,32 +1,47 @@
 package com.raydevelopers.newvalley.ui.fragments
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.raydevelopers.newvalley.R
+import com.raydevelopers.newvalley.databinding.AllChannelsFragmentBinding
+import com.raydevelopers.newvalley.dependencyinjector.DependencyProvider
 import com.raydevelopers.newvalley.viewmodels.AllChannelsViewModel
 
 class AllChannelsFragment : Fragment() {
-
+    private var mBinding:AllChannelsFragmentBinding? = null
     companion object {
         fun newInstance() =
             AllChannelsFragment()
     }
 
-    private lateinit var viewModel: AllChannelsViewModel
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // init data binding
+        mBinding =
+            DataBindingUtil.inflate(inflater, R.layout.all_channels_fragment, container, false)
+        // adding assertion as we have created object above and we are sure it will not be null
+        return mBinding!!.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(AllChannelsViewModel::class.java)
-        // TODO: Use the ViewModel
+        // make network request
+
     }
 
+    /**
+     * getViewModel() provides the object of viewmodel with the help [DependencyProvider]
+     */
+    private fun getViewModel(): AllChannelsViewModel {
+        return ViewModelProvider(this, DependencyProvider.provideLoginViewModelFactory()).get(
+            AllChannelsViewModel::class.java
+        )
+    }
 }
